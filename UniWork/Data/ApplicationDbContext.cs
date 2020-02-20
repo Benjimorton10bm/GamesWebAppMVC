a348ca040ba86a6c2c35b819d0aaecf13aff38ef
Benjamin Morton <benjamin.morton1@nhs.net>
2020-02-20 16:41:24 +0000

    added properties to the model classes

--- UniWork/Data/ApplicationDbContext.cs
+++ UniWork/Data/ApplicationDbContext.cs
@@ -12,5 +12,16 @@ namespace UniWork.Data
             : base(options)
         {
         }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Game>().ToTable("Game");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItem");
+        }
     }
 }
