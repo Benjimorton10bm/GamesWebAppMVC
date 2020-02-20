4c710245c15f7cc4b54cea1298fa3897634f4ca3
Benjamin Morton <benjamin.morton1@nhs.net>
2020-02-20 17:20:17 +0000

    updated the database

--- UniWork/Data/ApplicationDbContext.cs
+++ UniWork/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UniWork.Models;
 
 namespace UniWork.Data
 {
@@ -14,13 +15,13 @@ namespace UniWork.Data
         }
 
         public DbSet<Game> Games { get; set; }
-        public DbSet<Order> Orders { get; set; }
+        public DbSet<Orders> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Game>().ToTable("Game");
-            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Orders>().ToTable("Order");
             modelBuilder.Entity<OrderItem>().ToTable("OrderItem");
         }
     }
